acf81725c07ca3a1de96cf6d6dc9d9754e68415b
Ohcul94 <luchocaelli@gmail.com>
2026-07-12 04:45:15 -0300

    build: actualizacion automatica de codigo

--- deploy.bat
+++ deploy.bat
@@ -15,7 +15,7 @@ echo [2/5] Agregando cambios a Git...
 git add .
 
 echo [3/5] Creando commit local...
-git commit -m "build: actualizacion automatica de pcks y codigo"
+git commit -m "build: actualizacion automatica de codigo"
 
 echo [4/5] Subiendo cambios a GitHub...
 git push origin master
@@ -25,7 +25,20 @@ if %errorlevel% neq 0 (
     exit /b %errorlevel%
 )
 
-echo [5/5] Actualizando y reiniciando servidor Oracle Cloud...
+set /p UPLOAD_PCK="Deseas subir los archivos PCK (actualizaciones) por SCP a Oracle Cloud? (S/N): "
+if /i "%UPLOAD_PCK%"=="S" (
+    echo [5/6] Subiendo manifest.json y parches PCK directamente a Oracle Cloud...
+    scp -i "E:\Descon\OracleCloud\ssh-key-2026-04-20.key" Server/public/cdn/* ubuntu@138.2.241.76:~/Descon/Server/public/cdn/
+    if %errorlevel% neq 0 (
+        echo [ERROR] Error al subir los parches PCK por SCP.
+        pause
+        exit /b %errorlevel%
+    )
+) else (
+    echo [5/6] Omitiendo subida de archivos PCK por SCP.
+)
+
+echo [6/6] Actualizando y reiniciando servidor Oracle Cloud...
 ssh -i "E:\Descon\OracleCloud\ssh-key-2026-04-20.key" ubuntu@138.2.241.76 "cd ~/Descon && git pull origin master && pm2 restart mmo-server"
 if %errorlevel% neq 0 (
     echo [ERROR] Error al actualizar servidor Oracle Cloud.
